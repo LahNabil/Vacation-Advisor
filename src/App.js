597ed693dc1196e04a00a59beb842b04e44383f9
Nabil <lahlalia.nabil@hotmail.com>
2023-08-02 15:52:50 +0100

    list commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@
 import './App.css';
 import Header from './composants/Header/Header';
 import Map from './composants/Map/Map'
+import List from './composants/List/List';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Map/>
-    </div>
+      <div className='container'>
+        <div className='container1'>
+          <List/>
+        </div>
+        <div className='container2'>
+          <Map/>
+        </div>
+      </div>
+    </div>  
+    
+    
   );
 }
 
---
+++ src/composants/List/List.jsx
@@ -0,0 +1,51 @@
+import React, {useState} from 'react'
+import './style.css'
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@mui/material'
+import PlaceDetail from '../PlaceDetails/PlaceDetail'
+const List = () => {
+    const [type,setType] = useState('restaurants')
+    const [rating, setRating] = useState('')
+    const places = [
+        {name: 'Cool Place'},
+        {name: 'Best Coffee'},
+        {name: 'Zephyre'},
+        {name: 'Cool Place'},
+        {name: 'Best Coffee'},
+        {name: 'Zephyre'},
+        {name: 'Cool Place'},
+        {name: 'Best Coffee'},
+        {name: 'Zephyre'},
+    ];
+  return (
+    <div className='containerList'>
+        <Typography variant='h4'> Restaurants, Hotels & Attractions</Typography>
+
+        <FormControl className='formControl'>
+            <InputLabel>Type</InputLabel>
+            <Select value={type} onChange={(e)=> setType(e.target.value)} className='selectL'>
+                <MenuItem value="restaurants">Restaurants</MenuItem>
+                <MenuItem value="hotels">Hotels</MenuItem>
+                <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+        </FormControl>
+        <FormControl className='formControl'>
+            <InputLabel className='inputLabel'>Rating</InputLabel>
+            <Select value={rating} onChange={(e)=> setRating(e.target.value)} className='selectL'>
+                <MenuItem value={0}>All</MenuItem>
+                <MenuItem value={3}>Above 3</MenuItem>
+                <MenuItem value={4}>Above 4</MenuItem>
+                <MenuItem value={4.5}>Above 4.5</MenuItem>
+            </Select>
+        </FormControl>
+        <Grid container spacing={3} className='gridL'>
+            {places?.map((place, i )=>(
+                <Grid item key={i} xs={12}>
+                    <PlaceDetail place={place}/>
+                </Grid>
+            ))}
+        </Grid>
+    </div>
+  )
+}
+
+export default List
