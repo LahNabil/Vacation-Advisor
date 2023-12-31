a66f5bfc1613d4b315538d632c9916ca96bfb330
Nabil <lahlalia.nabil@hotmail.com>
2023-12-31 23:31:37 +0100

    sonnar code smells

--- src/composants/Map/Map.jsx
+++ src/composants/Map/Map.jsx
@@ -15,10 +15,7 @@ const Mapp = ({setCoordinates, setBounds, coordinates, places, setChildClicked})
   console.log({isDesktop})
   
     
-    // const {isLoaded} = useJsApiLoader({
-    //     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    // })
-    // if (!isLoaded) return <div>Loading ...</div>
+  
   return (
     <div>
         <div className='mapg'>
@@ -56,31 +53,14 @@ const Mapp = ({setCoordinates, setBounds, coordinates, places, setChildClicked})
                   />
                   <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
                   </Paper>
-                  // <Paper elevation={3} className='paper'>
-                  //   <Typography className='typography' variant='subtitle2' gutterBottom>
-                  //     {place.name}
-                  //   </Typography>
-                  //   <img src={place.photo ? place.photo.images.large.url: 'https://www.eatthis.com/wp-content/uploads/sites/4/2022/10/healthy-restaurant-dinner.jpg?quality=82&strip=1'}
-                  //         className='pointer'
-                  //         alt={place.name}
-                  //   />
-                  //   <Rating size="small" value={Number(place.rating)} readOnly />
-
-                  // </Paper>
+                
                 )}
               
 
             </div>
           ))}
         </GoogleMapReact>
-        {/* <GoogleMap onChange={(e)=>{
-          console.log(e)
-          setCoordinates({lat: e.center.lat, lng: e.center.lng})
-        }} zoom={10} margin={[50,50,50,50]} defaultCenter={center} defaultZoom={14} center={center} mapContainerStyle={{ width: '100%', height: '600px' }}>
-            <Marker position={{lat: 44, lng: -80}}/>
-        
-        
-        </GoogleMap> */}
+      
         </div>
         
     </div>
